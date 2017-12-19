32193d9aad664a36017b6e0a0d271bd4a170f734
Shubham Shukla <shk.shubham@gmail.com>
2017-12-19 02:55:30 +0000

    twits and sentiment working

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+const twitter = {
+  consumer_key: 'bW375fRdpElHkYrBGdJAVoeWA',
+  consumer_secret: 'KpIAL2z975Obk8Tfxv3ihsaF6AoT38PycCc5MSrrVPDONpoUpd',
+  access_token: '169575188-8hGjOSkCz8dM9lWQInz3zpCV17qliZebWIN4pmND',
+  access_token_secret: '1Gtk6RafS413Q6oAA0zMCtBVcv2BW57ZXfRWfjyucN3hR'
+}
+module.exports = {
+  twitter
+}
--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require('express')
+const sentiment = require('sentiment');
+const keys = require('./config/keys')
+const Twit = require('twit');
 const app = express()
 
 
-app.get('/', function(req, res){
-  res.send('home')
+var twitter = new Twit(keys.twitter)
+var twitsScore = []
 
-});
-
-app.get('/twitter', function(req, res){
-  res.send('twiiter')
-
-});
-
-app.listen(3000)
+app.post('/twitter', function(req, res){
+  var params = {
+    q: 'india',
+    count: 5
+  }
+  twitter.get('search/tweets', params,search)
+  function search(err, data, response) {
+    var twitsObj = {}
+    data.statuses.forEach(function (twits) {
+      var sent = sentiment(twits.text)
+      twitsObj = {
+        text: twits.text,
+        sentiment: sent
+      }
+      twitsScore.push(twitsObj)
+    })
+  }
+  res.send(twitsScore)
+  console.log(twitsScore)
+})
+app.listen(8000)
