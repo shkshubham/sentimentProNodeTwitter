55beb3a8f388d9cef0c86f7eadd1e356475b08eb
Shubham Shukla <shk.shubham@gmail.com>
2017-12-19 02:58:12 +0000

    routes and fetching data fixed

--- index.js
+++ index.js
@@ -4,28 +4,30 @@ const keys = require('./config/keys')
 const Twit = require('twit');
 const app = express()
 
-
 var twitter = new Twit(keys.twitter)
 var twitsScore = []
 
-app.post('/twitter', function(req, res){
-  var params = {
-    q: 'india',
-    count: 5
-  }
-  twitter.get('search/tweets', params,search)
-  function search(err, data, response) {
-    var twitsObj = {}
-    data.statuses.forEach(function (twits) {
-      var sent = sentiment(twits.text)
-      twitsObj = {
-        text: twits.text,
-        sentiment: sent
-      }
-      twitsScore.push(twitsObj)
-    })
-  }
-  res.send(twitsScore)
-  console.log(twitsScore)
-})
+var params = {
+  q: 'nodejs',
+  count: 50
+}
+
+twitter.get('search/tweets', params,search)
+
+function search(err, data, response) {
+  var twitsObj = {}
+  data.statuses.forEach(function (twits) {
+    var sent = sentiment(twits.text)
+    twitsObj = {
+      text: twits.text,
+      sentiment: sent
+    }
+    twitsScore.push(twitsObj)
+  })
+  app.get('/', function(req, res){
+    res.send(twitsScore)
+    console.log(twitsScore)
+  })
+}
+
 app.listen(8000)
